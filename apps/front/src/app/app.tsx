2feda0a7217691e08a14548b0168c34743c94aae
hans <hans@welinux.cl>
2024-07-11 11:56:41 -0400

    Authenticated pages

--- apps/front/src/app/app.tsx
+++ apps/front/src/app/app.tsx
@@ -1,40 +1,19 @@
 // src/App.jsx
 import React from 'react';
 import { useAuth } from 'react-oidc-context';
+import { Navbar } from './navbar';
+import { Authenticaded } from './Authenticaded';
+import { Unauthenticated } from './Unauthenticated';
 
-function App() {
+export function App() {
   const auth = useAuth();
-
-  switch (auth.activeNavigator) {
-    case 'signinSilent':
-      return <div>Signing you in...</div>;
-    case 'signoutRedirect':
-      return <div>Signing you out...</div>;
-  }
-
-  if (auth.isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (auth.error) {
-    return <div>Oops... {auth.error.message}</div>;
-  }
-
-  if (auth.isAuthenticated) {
-    return (
-      <div className="flex flex-col items-center ">
-        <div className="mb-4">Hello {auth.user?.profile.sub}</div>
-        <button className="btn" onClick={() => void auth.removeUser()}>
-          Log out
-        </button>
-      </div>
-    );
-  }
-
   return (
-    <button className="btn" onClick={() => void auth.signinRedirect()}>
-      Log in
-    </button>
+    <div>
+      <Navbar />
+      <div className="m-4">
+        {auth.isAuthenticated ? <Authenticaded /> : <Unauthenticated />}
+      </div>
+    </div>
   );
 }
 
--- apps/front/src/app/meta-app.tsx
+++ apps/front/src/app/meta-app.tsx
@@ -1,11 +1,10 @@
 import { AuthProvider } from 'react-oidc-context';
-import App from './app';
+import { App } from './app';
 
 const oidcConfig = {
   authority: import.meta.env.AUTHORITY || 'http://localhost:8080',
   client_id: import.meta.env.CLIENT_ID || '275414623035917827@zitadel-project',
-  redirect_uri:
-    import.meta.env.REDIRECT_URI || 'http://localhost:4200/admin/callback',
+  redirect_uri: import.meta.env.REDIRECT_URI || 'http://localhost:4200',
 };
 
 export function MetaApp() {
@@ -13,7 +12,7 @@ export function MetaApp() {
     <AuthProvider {...oidcConfig}>
       <div
         data-theme="cyberpunk"
-        className="flex h-screen flex-col items-center justify-center"
+        className="flex h-screen flex-col w-screen max-w-[1280px] m-auto"
       >
         <App />
       </div>
